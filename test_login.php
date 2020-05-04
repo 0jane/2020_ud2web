59c50ab7ad0f021437f04cbdcff2a0803b5ffc0d
devin liang <devinliang@gmail.com>
2020-05-04 15:48:16 +0800

    增加login測試

---
+++ test_login.php
@@ -0,0 +1,48 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登入測試</title>
+</head>
+<body>
+
+<?php
+    if (isset($_POST["submit"])) {
+
+        $_SESSION["username"] = $_POST["username"];
+        
+        echo "你好 " . $_SESSION["username"];
+
+        if ($_POST['prev'] != "") {
+           header('Location: ' . $_POST['prev']);
+           exit();
+        }
+
+        echo "歡迎參觀!";
+       
+    } else {
+        $from = "";
+        if (isset($_GET["from"])) $from=$_GET["from"];
+        
+?>
+
+<form action="" method="post">
+    <p>
+    帳號：<input type="text" name="username" id=""></p>
+    <p>
+    密碼：<input type="password" name="userpass" id=""></p>
+    <p>
+    <input type="hidden" name="prev" value="<?php echo $from; ?>">
+    <input type="submit" name="submit" value="登入">
+    </p>
+
+</form>
+
+<?php        
+    }
+?>
+
+</body>
+</html>
---
+++ test_member.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員頁面</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <?php 
+        if (isset($_GET["logout"]) && $_GET["logout"]=='1') {
+            unset($_SESSION["username"]);
+            session_destroy();
+            echo "尚未登入, 請先登入:<a href='test_login.php?from=".$_SERVER['PHP_SELF']."'>login</a>";
+        } else {
+            if (isset($_SESSION["username"]) && $_SESSION["username"]!="") {
+                echo $_SESSION["username"]."你好";
+                echo "<p>會員相關資訊...</p>";
+                echo "<p><a href=\"?logout=1\">登出系統</a></p>";
+            } else {
+                echo "尚未登入, 請先登入:<a href='test_login.php?from=".$_SERVER['PHP_SELF']."'>login</a>";
+            }
+        }
+    ?>
+</body>
+</html>
