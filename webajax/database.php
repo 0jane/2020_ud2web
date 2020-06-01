5a24d8e22fab001383b3c5a81c25d6b9e9e5ae87
devin liang <devinliang@gmail.com>
2020-06-01 16:58:03 +0800

    add php ajax crud example

---
+++ webajax/database.php
@@ -0,0 +1,7 @@
+<?php
+	$servername = "localhost";
+	$username = "school";
+	$password = "abc123";
+	$db="school";
+	$conn = mysqli_connect($servername, $username, $password,$db);
+?>
---
+++ webajax/index.php
@@ -0,0 +1,251 @@
+<?php
+session_start();
+
+include 'database.php';
+
+if (isset($_SESSION["username"]) && $_SESSION["username"]!="") {
+	$ulogin = TRUE;
+} else { $ulogin = FALSE; }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>User Data</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="ajax.js"></script>
+</head>
+<body>
+    <div class="container">
+	<p id="success"></p>
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+						<h2>Manage <b>Users</b></h2>
+					</div>
+					<div class="col-sm-6">
+						<?php if ($ulogin)	{ ?>
+
+						<a href="JavaScript:void(0);" class="btn btn-danger" id="delete_multiple"><i class="material-icons"></i> <span>刪除</span></a>
+						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons"></i> <span>新增</span></a>
+						<a href="#" class="btn btn-success" data-toggle="modal" id="logout"><i class="material-icons"></i> <span><?php echo $_SESSION['username']; ?> 登出</span></a>
+						
+						<?php } else { ?>
+						<a href="#userLoginModal" class="btn btn-success" data-toggle="modal"><i class="material-icons"></i> <span>登入</span></a>
+						<?php } ?>
+					</div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+						<th>
+							<span class="custom-checkbox">
+								<input type="checkbox" id="selectAll">
+								<label for="selectAll"></label>
+							</span>
+						</th>
+						<th>序號</th>
+                        <th>姓名</th>
+                        <th>EMAIL</th>
+						<th>PHONE</th>
+						<th>CITY</th>
+
+						<?php if ($ulogin) { ?>
+						<th>ACTION</th>
+						<?php } ?>
+
+                    </tr>
+                </thead>
+				<tbody>
+				
+				<?php
+				$result = mysqli_query($conn,"SELECT * FROM user_data");
+				$i=1;
+				while($row = mysqli_fetch_array($result)) {
+				?>
+				<tr id="<?php echo $row["id"]; ?>">
+				<td>
+							<span class="custom-checkbox">
+								<input type="checkbox" class="user_checkbox" data-user-id="<?php echo $row["id"]; ?>">
+								<label for="checkbox2"></label>
+							</span>
+						</td>
+					<td><?php echo $i; ?></td>
+					<td><?php echo $row["name"]; ?></td>
+					<td><?php echo $row["email"]; ?></td>
+					<td><?php echo $row["phone"]; ?></td>
+					<td><?php echo $row["city"]; ?></td>
+
+					<?php if ($ulogin) { ?>
+					<td>
+						<a href="#editEmployeeModal" class="edit" data-toggle="modal">
+							<i class="material-icons update" data-toggle="tooltip" 
+							data-id="<?php echo $row["id"]; ?>"
+							data-name="<?php echo $row["name"]; ?>"
+							data-email="<?php echo $row["email"]; ?>"
+							data-phone="<?php echo $row["phone"]; ?>"
+							data-city="<?php echo $row["city"]; ?>"
+							title="Edit"></i>
+						</a>
+						<a href="#deleteEmployeeModal" class="delete" data-id="<?php echo $row["id"]; ?>" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" 
+						 title="Delete"></i></a>
+					</td>
+					<?php } ?>
+					
+				</tr>
+				<?php
+				$i++;
+				}
+				?>
+				</tbody>
+			</table>
+			
+        </div>
+    </div>
+
+<!-- User Login Modal HTML -->
+<div id="userLoginModal" class="modal fade" aria-hidden="true" data-backdrop="static">
+		<div class="modal-dialog">
+			<div class="modal-content">
+			<div class="modal-header">				
+				<h4 class="modal-title">Member Login</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body">
+				<form action="" method="post">
+					<div class="form-group">
+						<i class="fa fa-user"></i>
+						<input type="text" class="form-control" placeholder="Username" required="required" id="username">
+					</div>
+					<div class="form-group">
+						<i class="fa fa-lock"></i>
+						<input type="password" class="form-control" placeholder="Password" required="required" id="userpass">
+					</div>
+					<div class="form-group">
+						<button type="button" class="btn btn-primary btn-block btn-lg" id="login_button">登入系統</button>						
+					</div>
+				</form>				
+				
+			</div>
+			<div class="modal-footer">
+				<a href="#">Forgot Password?</a>
+			</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- Add Modal HTML -->
+	<div id="addEmployeeModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form id="user_form">
+					<div class="modal-header">						
+						<h4 class="modal-title">Add User</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+					</div>
+					<div class="modal-body">					
+						<div class="form-group">
+							<label>NAME</label>
+							<input type="text" id="name" name="name" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>EMAIL</label>
+							<input type="email" id="email" name="email" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>PHONE</label>
+							<input type="phone" id="phone" name="phone" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>CITY</label>
+							<input type="city" id="city" name="city" class="form-control" required>
+						</div>					
+					</div>
+					<div class="modal-footer">
+					    <input type="hidden" value="1" name="type">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+						<button type="button" class="btn btn-success" id="btn-add">Add</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Edit Modal HTML -->
+	<div id="editEmployeeModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form id="update_form">
+					<div class="modal-header">						
+						<h4 class="modal-title">Edit User</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+					</div>
+					<div class="modal-body">
+						<input type="hidden" id="id_u" name="id" class="form-control" required>					
+						<div class="form-group">
+							<label>Name</label>
+							<input type="text" id="name_u" name="name" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Email</label>
+							<input type="email" id="email_u" name="email" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>PHONE</label>
+							<input type="phone" id="phone_u" name="phone" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>City</label>
+							<input type="city" id="city_u" name="city" class="form-control" required>
+						</div>					
+					</div>
+					<div class="modal-footer">
+					<input type="hidden" value="2" name="type">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+						<button type="button" class="btn btn-info" id="update">Update</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<!-- Delete Modal HTML -->
+	<div id="deleteEmployeeModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form>
+						
+					<div class="modal-header">						
+						<h4 class="modal-title">Delete User</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+					</div>
+					<div class="modal-body">
+						<input type="hidden" id="id_d" name="id" class="form-control">					
+						<p>Are you sure you want to delete these Records?</p>
+						<p class="text-warning"><small>This action cannot be undone.</small></p>
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+						<button type="button" class="btn btn-danger" id="delete">Delete</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
